b3b43a5e4cc6993f5bff6bd92cefe3b57f2e6449
june <jude.a.r.peel@gmail.com>
2024-11-14 18:16:10 -0700

    restructuring, and got huffman codes working albeit a bit messy.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
-use chameleon::compression;
-use chameleon::compression::huffman::Coder;
-use chameleon::compression::lzss::{self, LempelZiv};
+use chameleon::compression::huffman::{Coder, Huffman};
 use std::error::Error;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = fs::read("./data/sunbears.txt").unwrap();
 
-    let sunbear_huffman = compression::huffman::Huffman::encode(&file).unwrap();
+    let sunbear_huffman = Huffman::encode(&file).unwrap();
 
-    let sunbear_decoded = compression::huffman::Huffman::decode(&sunbear_huffman).unwrap();
+    let sunbear_decoded = Huffman::decode(&sunbear_huffman).unwrap();
 
     println!(
-        "Before: {} bytes\n After: {} bytes\n Decoded: {}",
+        "Before: {} bytes\n After: {} bytes\n Decoded: {} bytes",
         file.len(),
         sunbear_huffman.len(),
         sunbear_decoded.len()
     );
 
-    println!("{:?}", (file == sunbear_decoded));
+    println!("Before == Decoded: {:?}", (file == sunbear_decoded));
     Ok(())
 }
